7f10a80c69446fc584810d2230a3efea37b96b6b
AnthonyRo1 <anthonyseo556@gmail.com>
2022-07-13 08:13:56 -0700

    this is a commit for deployment

--- migrations/versions/c3fa08b25031_.py
+++ migrations/versions/c3fa08b25031_.py
@@ -1,8 +1,8 @@
-"""migrate
+"""empty message
 
-Revision ID: 0d534f4e7dc5
+Revision ID: c3fa08b25031
 Revises: 
-Create Date: 2022-07-11 22:29:41.777030
+Create Date: 2022-07-13 08:13:19.627434
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '0d534f4e7dc5'
+revision = 'c3fa08b25031'
 down_revision = None
 branch_labels = None
 depends_on = None
